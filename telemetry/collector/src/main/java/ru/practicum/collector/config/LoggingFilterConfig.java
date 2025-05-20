18cf081f60abd3ebe91ed7d52ae4ebcece564ede
Boris <boris.z.077@gmail.com>
2025-05-21 01:18:10 +0300

    added location

---
+++ telemetry/collector/src/main/java/ru/practicum/collector/config/LoggingFilterConfig.java
@@ -0,0 +1,20 @@
+package ru.practicum.collector.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+@Profile("main-service")
+public class LoggingFilterConfig {
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setMaxPayloadLength(10000);
+        return loggingFilter;
+    }
+}
